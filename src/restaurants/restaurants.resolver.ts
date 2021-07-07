01a3b6304d2c10e6a3c4b1a7eb34eee3d72dea7e
jerry-sunm <jisun124918@gmail.com>
2021-07-07 12:22:40 +0900

    graphql api

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurants.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
