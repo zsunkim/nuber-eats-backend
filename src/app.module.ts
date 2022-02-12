1fa90cb2cb4fa1e4bd1191f952d9c306ac68cfd2
jerry-sunm <66250890+jerry-sunm@users.noreply.github.com>
2022-02-12 22:42:09 +0900

    Add configModule and typeOrmModule to Appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".dev.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      // 내가 원하는 모든 환경 변수의 유효성을 검사할 수 있음.
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'test', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, // database를 모듈의 현재 상태로 마이그래이션
+      logging: true, // database에서 일어나는 것을 콘솔로 표시
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
@@ -13,4 +40,4 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
