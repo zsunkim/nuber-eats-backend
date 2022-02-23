3a7231e19bdf74f57358e6a3a7d32bdb8497f766
zsunkim <jisun124918@gmail.com>
2022-02-23 22:27:04 +0900

    import user module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import * as Joi from 'joi';
 import { Restaurant } from './restaurants/entities/restaurants.entity';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -32,12 +35,13 @@ import { Restaurant } from './restaurants/entities/restaurants.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== "prod", // database를 모듈의 현재 상태로 마이그래이션
       logging: process.env.NODE_ENV !== "prod", // database에서 일어나는 것을 콘솔로 표시
-      entities: [Restaurant]
+      entities: [User]
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
