ee14779ea486526c0dccb84a0cf27d00a4852702
zsunkim <jisun124918@gmail.com>
2022-02-26 22:09:00 +0900

    role type 변경 (enum)

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,12 +1,16 @@
+import { Field } from "@nestjs/graphql";
 import { CreateDateColumn, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
 export class CoreEntity {
   @PrimaryColumn()
+  @Field(type => String)
   id: number;
 
   @CreateDateColumn()
+  @Field(type => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Field(type => Date)
   updatedAt: Date;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,17 +1,28 @@
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
 
-type UserRole = "client" | "owner" | "delivery"
+enum UserRole {
+  Client,
+  Wwner,
+  Delivery
+}
 
+registerEnumType(UserRole, { name: "UserRole" })
+
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field(type => String)
   email: string;
 
   @Column()
+  @Field(type => String)
   password: string;
 
-  // client/owner/delivery
-  @Column()
+  @Column({ type: 'enum', enum: UserRole }) // 데이터베이스에는 type이 enum인 UserRole
+  @Field(type => UserRole) // type이 UserRole인 graphQL
   role: UserRole;
 }
