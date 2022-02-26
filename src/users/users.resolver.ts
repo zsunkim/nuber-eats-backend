bc56327c02016221b303876e8d0a3871d47b8f9c
zsunkim <jisun124918@gmail.com>
2022-02-26 22:54:38 +0900

    return 값 변경

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,6 @@
 
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { createAccountInput } from "./dtos/create-account.dto";
+import { createAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -15,8 +15,20 @@ export class UsersResolver {
     return true;
   }
 
-  @Mutation(returns => Boolean)
-  createAccount(@Args("input") createAccountInput: createAccountInput) {
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(@Args("input") createAccountInput: createAccountInput): Promise<CreateAccountOutput> {
+    try {
+      const [ok, error] = await this.usersService.createAccount(createAccountInput);
 
+      return {
+        ok,
+        error
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error
+      }
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,19 +9,18 @@ export class UsersService {
     private readonly user: Repository<User>,
   ) { }
 
-  async createAccount({ email, password, role }: createAccountInput) {
+  async createAccount({ email, password, role }: createAccountInput): Promise<[boolean, string?]> {
     // 유저 확인 -> 없으면 유저 생성 & 비밀번호 hash
     try {
       const exists = await this.user.findOne({ email });
       if (exists) {
-
-        return;
+        return [false, '이미 존재하는 이메일입니다.'];
       }
       await this.user.save(this.user.create({ email, password, role }));
-      return true;
+      return [true]
     } catch (error) {
       console.log(error);
-      return error.message;
+      return [false, '생성 실패'];
     }
 
   }
