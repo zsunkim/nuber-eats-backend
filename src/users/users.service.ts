d462aa458df0844212be3bdda11cef3ef333528d
zsunkim <jisun124918@gmail.com>
2022-02-26 22:09:57 +0900

    create account service 생성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { createAccountInput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 export class UsersService {
@@ -7,4 +8,21 @@ export class UsersService {
     @InjectRepository(User)
     private readonly user: Repository<User>,
   ) { }
+
+  async createAccount({ email, password, role }: createAccountInput) {
+    // 유저 확인 -> 없으면 유저 생성 & 비밀번호 hash
+    try {
+      const exists = await this.user.findOne({ email });
+      if (exists) {
+
+        return;
+      }
+      await this.user.save(this.user.create({ email, password, role }));
+      return true;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+
+  }
 }
