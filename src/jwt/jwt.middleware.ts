3495ad73c827a13fb0b0c1079a2a52e862da3677
zsunkim <jisun124918@gmail.com>
2022-03-06 23:13:49 +0900

    modify jwt middleware (token verify)

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,14 +1,31 @@
-import { NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
+import { UsersService } from "src/users/users.service";
+import { JwtService } from "./jwt.service";
 
-// export class JwtMiddleware implements NestMiddleware {
-//   use(req: Request, res: Response, next: NextFunction) {
-//     console.log(req.headers);
-//     next();
-//   }
-// }
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) { }
 
-export function JwtMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(req.headers);
-  next();
+  async use(req: Request, res: Response, next: NextFunction) {
+    console.log(req.headers);
+    if ("x-jwt" in req.headers) {
+      console.log(req.headers['x-jwt']);
+      const token = req.headers['x-jwt'];
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === "object" && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.userService.findById(decoded['id']);
+          req['user'] = user;
+        } catch (error) {
+          console.log(error);
+          throw new Error(error);
+        }
+      }
+    }
+    next();
+  }
 }
