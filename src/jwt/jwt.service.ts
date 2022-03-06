f221d2981d6aa4f5ea2a030cd1288b0cb25101a3
zsunkim <jisun124918@gmail.com>
2022-03-06 22:28:51 +0900

    create sign to token payload

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,10 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class JwtService {
   constructor(
-    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions, // configService로도 사용가능
   ) { }
+
+  sign(payload: object): string {
+    console.log(payload);
+    return jwt.sign(payload, this.options.privateKey);
+  }
 }
