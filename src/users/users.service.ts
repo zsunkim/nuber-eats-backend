2d93c5bf24ea1119bbc788fcefae7fb05110dc02
zsunkim <jisun124918@gmail.com>
2022-03-08 23:42:02 +0900

    edit profile service 생성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { JwtService } from "src/jwt/jwt.service";
+import { EditProfileInput } from "./dtos/edit-profile.dto";
 
 export class UsersService {
   constructor(
@@ -58,5 +59,9 @@ export class UsersService {
 
   async findById(id: number): Promise<User> {
     return this.userRepository.findOne({ id });
+  }
+
+  async editProfile(userId: number, { email, password }: EditProfileInput) {
+    return this.userRepository.update(userId, { email, password });
   }
 }
