fc16819ea2bcda519cfc69e5eeb3c8b4c30ea23e
zsunkim <jisun124918@gmail.com>
2022-03-09 23:04:30 +0900

    typeorm verification entity 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([User, Verification])],
   providers: [UsersResolver, UsersService],
   exports: [UsersService]
 })
