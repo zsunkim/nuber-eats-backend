f6c56f8e6184fe778d3fe841b6cdbdcc04535077
zsunkim <jisun124918@gmail.com>
2022-03-14 16:00:16 +0900

    common constants 생성

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 import * as jwt from 'jsonwebtoken';
+import { CONFIG_OPTIONS } from 'src/common/entities/common.constants';
 
 @Injectable()
 export class JwtService {
