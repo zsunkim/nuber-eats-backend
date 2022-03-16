295b2862575a042c58290dad9f5c971c51e2c2fa
zsunkim <jisun124918@gmail.com>
2022-03-16 23:32:06 +0900

    create send mail service

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,35 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { MailModuleInterface } from './mail.interface';
+import { CONFIG_OPTIONS } from 'src/common/entities/common.constants';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleInterface,
+  ) {
+    this.sendEmail('testing', 'test');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `zsunn96@gmail.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
