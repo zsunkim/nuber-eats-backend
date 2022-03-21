a21c56e7f6c245da5779fef823698d085b5bb575
zsunkim <jisun124918@gmail.com>
2022-03-21 23:21:42 +0900

    add email interface

--- src/mail/mail.interface.ts
+++ src/mail/mail.interface.ts
@@ -1,5 +1,10 @@
 export interface MailModuleInterface {
-    apiKey: string;
-    domain: string;
-    fromEmail: string;
+  apiKey: string;
+  domain: string;
+  fromEmail: string;
+}
+
+export interface EmailVar {
+  key: string;
+  value: string;
 }
