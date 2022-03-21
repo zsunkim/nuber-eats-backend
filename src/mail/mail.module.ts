2b49e8c164c23ed62689b1369e1555761bcacc6e
zsunkim <jisun124918@gmail.com>
2022-03-21 23:23:58 +0900

    add global decorator to mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,9 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/entities/common.constants';
 import { MailModuleInterface } from './mail.interface';
 
 @Module({})
+@Global()
 export class MailModule {
   static forRoot(options: MailModuleInterface): DynamicModule {
     return {
